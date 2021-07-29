bc73b686cf281bbbc28c147a8d5a086244b0a62e
tvsherstneva <tv.sherstneva@yandex.ru>
2021-07-29 17:57:10 +0300

    final result

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 'use strict';
-
 function calculateProfit(amount, period, percent) {
     //amount — первоначальная сумма вклада (капитал),
-    //period — количество месяцев начисления процентов по привлеченному вкладу,
+    //period — срок вклада
     const countMonth = 1; //количество месяцев начисления процентов по привлеченному вкладу
     const numberPeriods = period / 1;//— число периодов начисления процентов.
     const totalMonthsYear = 12;//— количество месяцев в календарном году
     return amount * (Math.pow((1 + percent * countMonth / totalMonthsYear / 100), numberPeriods) - 1); //расчет суммы процентов (дохода)
 }
-
 function listenerSubmit(evt) {
     evt.preventDefault();
     totalEl.textContent = '';
@@ -27,33 +25,30 @@ function listenerSubmit(evt) {
     const maxAmount = 50000000;
     const amountInput = Number(amountInputEl.value);
     if (Number.isNaN(amountInput)) {
-        amountErrorEl.textContent = `Сумма – не число: "Неверное значение. Введите число, например: 15000`;
+        amountErrorEl.textContent = 'Неверное значение. Введите число, например: 15000';
         return;
     }
-
     if (amountInput < minAmount) {
-        amountErrorEl.textContent = `Сумма меньше минимальной: "Неверное значение. Минимальная сумма: ${minAmount} ₽`;
+        amountErrorEl.textContent = `Неверное значение. Минимальная сумма: ${minAmount} ₽`;
         return;
     }
     if (amountInput > maxAmount) {
-        amountErrorEl.textContent = `Сумма больше максимальной: "Неверное значение. Максимальная сумма: ${maxAmount} ₽`;
+        amountErrorEl.textContent = `Неверное значение. Максимальная сумма: ${maxAmount} ₽`;
         return;
     }
-
     const minPeriod = 3;
     const maxPeriod = 18;
     const periodInput = Number(periodInputEl.value);
     if (Number.isNaN(periodInput)) {
-        periodErrorEl.textContent = `Период – не число: "Неверное значение. Введите число месяцев, например: 3`;
+        periodErrorEl.textContent = 'Неверное значение. Введите число месяцев, например: 3';
         return;
     }
-
     if (periodInput < minPeriod) {
-        periodErrorEl.textContent = `Период меньше минимального: "Неверное значение. Минимальный период: ${minPeriod} месяца`;
+        periodErrorEl.textContent = `Неверное значение. Минимальный период: ${minPeriod} месяца`;
         return;
     }
     if (periodInput > maxPeriod) {
-        periodErrorEl.textContent = `Период больше максимального: "Неверное значение. Максимальный период: ${maxPeriod} месяцев`;
+        periodErrorEl.textContent = `Неверное значение. Максимальный период: ${maxPeriod} месяцев`;
         return;
     }
     const percent = percents[periodInput];
@@ -63,7 +58,6 @@ function listenerSubmit(evt) {
     profitEl.textContent = `${profit}`;
     percentEl.textContent = percent;
 }
-
 const formEl = document.getElementById('deposit-form');
 formEl.addEventListener('submit', listenerSubmit);
 const amountInputEl = document.getElementById('amount-input');
@@ -73,3 +67,4 @@ const profitEl = document.getElementById('profit');
 const percentEl = document.getElementById('percent');
 const amountErrorEl = document.getElementById('amount-error');
 const periodErrorEl = document.getElementById('period-error');
+
