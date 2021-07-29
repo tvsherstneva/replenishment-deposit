fca7814ddd5492ed828577a06a3858b8c7ef982f
tvsherstneva <tv.sherstneva@yandex.ru>
2021-07-29 12:56:01 +0300

    calculate

---
+++ js/app.js
@@ -0,0 +1,41 @@
+function calculateProfit(amount, period, percent) {
+    //amount — первоначальная сумма вклада (капитал),
+    //period — количество месяцев начисления процентов по привлеченному вкладу,
+    const N = 1;//— число периодов начисления процентов.??????
+    const totalMonthsYear = 12;//— количество месяцев в календарном году
+    return amount * (Math.pow((1 + percent * period / totalMonthsYear / 100), N) - 1); //расчет суммы процентов (дохода)
+}
+
+function listenerSubmit(evt) {
+    evt.preventDefault();
+    totalEl.textContent = '';
+    profitEl.textContent = '';
+    percentEl.textContent = '';
+    const percents = {
+        3: 2,
+        6: 2.2,
+        9: 2.3,
+        12: 2.6,
+        18: 2.7,
+    }
+    const amountInput = Number(amountInputEl.value);
+    const periodInput = Number(periodInputEl.value);
+    const percent = percents[periodInput];
+    let profit = calculateProfit(amountInput, periodInput, percent);
+  //  profit = Number(profit.toFixed());
+    totalEl.textContent = amountInput + profit;
+    profitEl.textContent = profit;
+    percentEl.textContent = percent;
+
+}
+
+const formEl = document.getElementById('deposit-form');
+formEl.addEventListener('submit', listenerSubmit);
+
+//задается
+const amountInputEl = document.getElementById('amount-input'); //сумма вклада(К — первоначальная сумма вклада (капитал))
+const periodInputEl = document.getElementById('period-input');//d — количество дней(месяцев) начисления процентов по привлеченному вкладу,
+//надо будет вывести
+const totalEl = document.getElementById('total');//S — сумма вклада с процентами,
+const profitEl = document.getElementById('profit');//Sp — сумма процентов (доход),
+const percentEl = document.getElementById('percent');//P — годовая процентная ставка,
